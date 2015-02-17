4a325caa97de9c5ff2f54bd088e390778c6aad16
Eric Stensland <estensland88@gmail.com>
2015-02-16 21:56:52 -0600

    logic for the inner box to not move outside of the bigger one

--- app/scripts/components/box/view.js
+++ app/scripts/components/box/view.js
@@ -26,25 +26,38 @@ define([
       baseProto.initialize.apply(this, arguments);
 
       $(document).on('keyup', function(e) {
+        var amount = '20'
         switch (e.which) {
         case 37:
+            if (parseFloat($('.moving-box').css('left').replace(/[a-z]*/g, '')) < 21){
+              amount = $('.moving-box').css('left').replace(/[a-z]*/g, '')
+            }
             $('.moving-box').stop().animate({
-                left: '-=20'
+                left: '-=' + amount
             }); //left arrow key
             break;
         case 38:
+            if (parseFloat($('.moving-box').css('top').replace(/[a-z]*/g, '')) < 21){
+              amount = $('.moving-box').css('top').replace(/[a-z]*/g, '')
+            }
             $('.moving-box').stop().animate({
-                top: '-=20'
+                top: '-=' + amount
             }); //up arrow key
             break;
         case 39:
+            if (parseFloat($('.moving-box').css('left').replace(/[a-z]*/g, '')) > 678){
+              amount = 698 - parseFloat($('.moving-box').css('left').replace(/[a-z]*/g, ''))
+            }
             $('.moving-box').stop().animate({
-                left: '+=20'
+                left: '+=' + amount.toString()
             }); //right arrow key
             break;
         case 40:
+            if (parseFloat($('.moving-box').css('top').replace(/[a-z]*/g, '')) > 328){
+              amount = 348 - parseFloat($('.moving-box').css('top').replace(/[a-z]*/g, ''))
+            }
             $('.moving-box').stop().animate({
-                top: '+=20'
+                top: '+=' + amount.toString()
             }); //bottom arrow key
             break;
         }
